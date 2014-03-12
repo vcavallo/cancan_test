9a8f9d70a5350a65014eb2e677e113e8854d4900
vcavallo <vcavallo@gmail.com>
2014-03-12 16:52:38 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CancanTest::Application.config.secret_key_base = '1e97644817a51c77fd7e6223567f10c7d65211581680dd0cf8ce67b07a59e2ac84bd0cb7521879585b9679d07c3a62696d0ac786dd523ef74bc75261ecde639b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CancanTest::Application.config.session_store :cookie_store, key: '_cancan_test_session'
